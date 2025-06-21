d16cce97672fb260fabd3b319a05153a187369bb
must_eat <eateing@loggedin.net>
2025-06-21 21:52:42 +0300

    upd: (gencloud/) cli.py; remove all libvirt driver interactions.

--- gencloud/cli.py
+++ gencloud/cli.py
@@ -2,7 +2,6 @@ import argparse
 import logging
 
 from .config import ConfigYAML
-from .driver import APIDriver
 from .log import set_root_logger
 from .mkuser import MkUser
 from .util import ask_q
@@ -17,30 +16,12 @@ class CLI:
         self.args = None
         self.subparsers = None
 
-        self.driver = None
-
     # - - parsing - - #
     def _mkuser_args(self):
         mkuser_subparser_desc = "create a UserSpec yaml to be consumed by --userspec"
 
         self.subparsers.add_parser(
             "mkuser", help=mkuser_subparser_desc, description=mkuser_subparser_desc
-        )
-
-    def _nuke_args(self):
-        nuke_subparser_desc = "nuke a vm"
-        nuke_subparser_name_help = "name of the domain to be nuked"
-        nuke_subparser_noconfirm_help = "skip the confirmation dialogue"
-
-        nuke_subparser = self.subparsers.add_parser(
-            "nuke", help=nuke_subparser_desc, description=nuke_subparser_desc
-        )
-        nuke_subparser.add_argument("name", type=str, help=nuke_subparser_name_help)
-        nuke_subparser.add_argument(
-            "--noconfirm",
-            action="store_true",
-            required=False,
-            help=nuke_subparser_noconfirm_help,
         )
 
     def _create_args(self):
@@ -82,35 +63,8 @@ class CLI:
         self.subparsers = parser.add_subparsers(dest="command", required=True)
 
         self._create_args()
-        self._nuke_args()
         self._mkuser_args()
         self.args = parser.parse_args()
-
-    # - - driver actions - - #
-    def _nuke(self):
-        if self.args.noconfirm:
-            want_nuke = True
-        else:
-            while True:
-                consent = (
-                    ask_q(f"do you want {self.args.name} nuked? (y/n)")
-                    .lower()
-                    .strip(" ")
-                )
-
-                want_nuke = (
-                    True if consent == "y" else False if consent == "n" else None
-                )
-
-                if want_nuke is None:
-                    self.logger.warning("input either `y' or `n'.")
-                else:
-                    break
-
-        if want_nuke:
-            self.driver.nuke(self.args.name)
-        else:
-            self.logger.warning("user cancelled action, bailing out.")
 
     def _create(self):
         config = ConfigYAML(
@@ -124,8 +78,6 @@ class CLI:
             err_msg = "no users or user-data file was provided, bailing out as "
             err_msg += "you may be unable to log in to an VMs created"
             self.logger.error(err_msg)
-
-        self.driver.create(config.vmspec)
 
     # - - main - - #
     def run(self):
@@ -141,18 +93,8 @@ class CLI:
             mku = MkUser()
             return mku.run()
 
-        # - - driver action - - #
-        self.driver = APIDriver()
-        self.driver.connect()
-
         if self.args.command == "create":
             self._create()
-        elif self.args.command == "nuke":
-            self._nuke()
-
-        self.logger.info("closing connection to the libVirt API")
-        self.driver.close()
-
 
 def run():
     c = CLI()
