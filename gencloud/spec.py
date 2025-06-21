a9fc1b89b68485ed84fa28d71ecf7eeebdd30f98
must_eat <eateing@loggedin.net>
2025-06-21 21:53:50 +0300

    upd: (gencloud/) spec.py; remove all vm resource def attributes except for where related to cloud-init YAMLs.

--- gencloud/spec.py
+++ gencloud/spec.py
@@ -2,22 +2,6 @@ class VMSpec:
     def __init__(self):
         # domain
         self.dom_name = None
-        self.dom_mem = None
-        self.dom_vcpu = None
-
-        # networking
-        self.net = None
-        self.mac_addr = None
-        self.ip = None
-        self.gateway = None
-        self.bridge_pfxlen = None
-        self.isolated_port = None
-
-        # storage
-        self.vol_pool = None
-        self.vol_size = None
-        self.vol_name = None
-        self.base_image = None
 
         # misc
         self.sshpwauth = None
